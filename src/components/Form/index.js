75592ca7b1d17a49d61d3a389e70ccc3776eaaf8
psmigielski <walek.smigielski@gmail.com>
2020-12-06 13:52:43 +0100

    add Form and Arrow components

---
+++ src/components/Form/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./index.scss";
+import { Link } from "react-router-dom";
+
+const Form = ({
+  wrapperHeight,
+  formHeight,
+  label,
+  messages,
+  children,
+  handleSubmit,
+}) => {
+  let notification = "";
+  let errorMessage = "";
+  return (
+    <div className="formContainer" style={{ height: wrapperHeight }}>
+      <p className="formContainer__header">{label}</p>
+      <form
+        className="form"
+        style={{ height: formHeight }}
+        onSubmit={handleSubmit}
+      >
+        {children}
+        <p className="error">{errorMessage}</p>
+        <p className="notification">{notification}</p>
+        <button type="submit" className="form__submit">
+          {label}
+        </button>
+      </form>
+      <div className="message">
+        {messages === "Login" && (
+          <p className="formContainer__paragraph">
+            Zapomniałeś hasło?<Link to="/reset-password"> Kliknij tutaj</Link>{" "}
+          </p>
+        )}
+        {messages === "Login" && (
+          <p className="formContainer__paragraph">
+            Nie masz konta?<Link to="/register"> Zarejestruj się</Link>
+          </p>
+        )}
+        {messages === "Register" && (
+          <p className="formContainer__paragraph">
+            Masz już konto?<Link to="/login">Zaloguj się</Link>
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Form;
