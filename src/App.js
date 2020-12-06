cca53ca8b97512d5ecf10ac1c7b88dc007bf5cef
psmigielski <walek.smigielski@gmail.com>
2020-12-06 13:59:05 +0100

    add LoginForm view

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import { Switch, Route, useLocation } from "react-router-dom";
 import "./App.scss";
 import Home from "./views/Home";
+import LoginForm from "./views/LoginForm";
 
 function App() {
   const location = useLocation();
   return (
     <Switch location={location} key={location.key}>
-      <Route path="/">
+      <Route path="/" exact>
         <Home />
+      </Route>
+      <Route path="/login">
+        <LoginForm />
       </Route>
     </Switch>
   );
