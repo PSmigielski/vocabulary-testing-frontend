53b8046809b928f8b03db9eef1d33d09fa7b4595
psmigielski <walek.smigielski@gmail.com>
2020-12-06 13:51:37 +0100

    addn useInput hook

---
+++ src/hooks/useInput.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const useInput = (initValue) => {
+  const [value, setValue] = useState(initValue);
+  const reset = () => {
+    setValue(initValue);
+  };
+  const bind = {
+    value,
+    onChange: (e) => setValue(e.target.value),
+  };
+  return [value, bind, reset];
+};
+
+export default useInput;
