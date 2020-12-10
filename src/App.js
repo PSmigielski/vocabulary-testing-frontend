ecf2179e31ce5ffc9a12efd02a784dd544941822
psmigielski <walek.smigielski@gmail.com>
2020-12-10 22:59:09 +0100

    add MessageProvider to app

--- src/App.js
+++ src/App.js
@@ -8,36 +8,39 @@ import RegisterForm from "./views/RegisterForm";
 import ResetPasswordForm from "./views/ResetPasswordForm";
 import VerifyPage from "./views/VerifyPage";
 import { UserProvider } from "./contexts/UserContext";
+import { MessageProvider } from "./contexts/MessageContext";
 
 function App() {
   const location = useLocation();
   const history = useHistory();
   return (
-    <UserProvider>
-      <Switch location={location} key={location.key}>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/login">
-          <LoginForm history={history} />
-        </Route>
-        <Route path="/register">
-          <RegisterForm />
-        </Route>
-        <Route path="/forgot">
-          <ForgotPasswordForm />
-        </Route>
-        <Route path="/reset">
-          <ResetPasswordForm />
-        </Route>
-        <Route
-          path="/verify/:login"
-          render={(routerProps) => {
-            return <VerifyPage routerProps={routerProps} />;
-          }}
-        />
-      </Switch>
-    </UserProvider>
+    <MessageProvider>
+      <UserProvider>
+        <Switch location={location} key={location.key}>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/login">
+            <LoginForm history={history} />
+          </Route>
+          <Route path="/register">
+            <RegisterForm history={history} />
+          </Route>
+          <Route path="/forgot">
+            <ForgotPasswordForm />
+          </Route>
+          <Route path="/reset">
+            <ResetPasswordForm />
+          </Route>
+          <Route
+            path="/verify/:login"
+            render={(routerProps) => {
+              return <VerifyPage routerProps={routerProps} />;
+            }}
+          />
+        </Switch>
+      </UserProvider>
+    </MessageProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:4100/v1/api";
+axios.defaults.baseURL = "http://localhost:4000/v1/api";
 ReactDOM.render(
   <React.StrictMode>
     <Router>
