ddf8b228e1e965d6258f868cf1af9305e6cde1ac
psmigielski <walek.smigielski@gmail.com>
2021-01-11 23:19:23 +0100

    add handleLogout method to Nav and fixed bugs

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,18 +1,32 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useHistory } from "react-router-dom";
 import "./index.scss";
+import { AuthContext } from "../../contexts/AuthContext"; 
 import Logo from "../Logo";
+import axios from "axios";
 
-const Nav = ({ isLoggedIn }) => {
+const Nav = () => {
+  const auth = useContext(AuthContext)
+  let isLoggedIn = auth.isLoggedIn;
+  const history = useHistory();
+  const handleLogout = () => {
+    axios.delete('/user/logout').then(res => {
+      history.push('/');
+      auth.logout();
+      auth.setIsLoggedIn(false);
+    }).catch(err => {
+      alert(err.response.data.message)
+    })
+  }
   return (
     <div className="nav__wrapper">
       <div className="nav__container">
         <Logo />
-        <div style={isLoggedIn ? "" : { display: "none" }} className="menu">
-          <Link to="/dashboard">Lekcje</Link>
+        <div style={{display: isLoggedIn ? 'flex' : 'none'}} className="menu">
+          <Link to="/dashboard">Testy</Link>
           <Link to="/stats">Statystyki</Link>
-          <Link to="/options">opcje</Link>
-          <button className="menu__button">Wyloguj</button>
+          <Link to="/options">Opcje</Link>
+          <button onClick={handleLogout} className="menu__button">Wyloguj</button>
         </div>
       </div>
     </div>
