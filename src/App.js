c39195b192e98da1e27e6198b330e68d139ff12d
psmigielski <walek.smigielski@gmail.com>
2021-01-13 15:53:56 +0100

    add getting _csrf cookie

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
 import AppRoutes from "./components/AppRoutes";
 import { AuthProvider } from "./contexts/AuthContext";
 import { MessageProvider } from "./contexts/MessageContext";
+import axios from 'axios';
 function App() {
-
+  useEffect(()=>{
+    const getCsrfToken = async () =>{
+      const {data} = await axios.get('/user/csrf-token');
+      axios.defaults.headers['X-CSRF-TOKEN'] = data.csrfToken
+    }
+    getCsrfToken()
+  },[])
   return (
     <MessageProvider>
       <AuthProvider>
